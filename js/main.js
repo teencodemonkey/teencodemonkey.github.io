c05a590d6578802bb6719f2477a92dafa1254bfb
Yash Patel <Yash Patel>
2016-12-24 22:24:48 -0500

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-$(function(){
-  $('#links li a').each(function() {
-    if ($(this).prop('href') == window.location.href) {
-      $(this).addClass('active');
-    }
-  });
-});
+var app = angular.module('blogPage', []);
+
+app.controller('linkController', ['$scope', function ($scope) {
+
+    $scope.templateURL = 'home.html';
+
+    $scope.showHome = function () {
+        $scope.templateURL = 'home.html';
+    };
+    $scope.showAbout = function () {
+        $scope.templateURL = 'about.html';
+    };
+    $scope.showProjects = function () {
+        $scope.templateURL = 'projects.html';
+    };
+
+    $scope.isActive = function(route) {
+        return route === $scope.templateURL;
+    };
+
+
+}]);
